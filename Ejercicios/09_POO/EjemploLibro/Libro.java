93128949e6f941afede1b7d7c40cc73bf81b8133
Gianluca Maida <124819060+Giaan46@users.noreply.github.com>
2024-03-22 20:43:08 +0100

    Add files via upload

---
+++ Ejercicios/09_POO/EjemploLibro/Libro.java
@@ -0,0 +1,12 @@
+/**
+ * Libro.java
+ * Definición de la clase Libro
+ * @author Gianluca Maida
+ */
+public class Libro {
+  // atributos
+  String isbn;
+  String titulo;
+  String autor;
+  int numeroDePaginas;
+}
