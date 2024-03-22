e5f9027188a68e7348e988aa077fe13286e84340
Gianluca Maida <124819060+Giaan46@users.noreply.github.com>
2024-03-22 20:44:37 +0100

    Add files via upload

---
+++ Ejercicios/09_POO/EjemploAnimal/Ave.java
@@ -0,0 +1,29 @@
+/**
+ * Ave.java
+ * Definición de la clase Ave
+ * @author Gianluca Maida
+ */
+public class Ave extends Animal {
+  
+  public Ave(Sexo s) {
+    super(s);
+  }
+
+  public Ave() {
+    super();
+  }
+
+  /**
+   * Hace que el ave se limpie.
+   */  
+  public void aseate() {
+    System.out.println("Me estoy limpiando las plumas");
+  }
+
+  /**
+   * Hace que el ave levante el vuelo.
+   */  
+  public void vuela() {
+    System.out.println("Estoy volando");
+  }
+}
